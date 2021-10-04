772c0376653c1591ff3f85cda05a25e75f2ffda3
alwihaque <alwihaque@icloud.com>
2021-10-04 15:27:30 -0400

    Fixed scheduler for scraping

--- Util/timer.js
+++ Util/timer.js
@@ -1,4 +1,8 @@
 const scraper = require("../Util/scrape");
+const cron = require('node-cron');
 module.exports.scrapeEvery12Hrs = async () => {
-    await scraper.getData();
+    cron.schedule('0 */12 * * *', scraper.getData, {
+        scheduled: true,
+        timezone: "America/Sao_Paulo"
+    });
 }
