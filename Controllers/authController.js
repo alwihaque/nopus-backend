334501ce273612d2cebebff1da365055bd8f98d3
enorytk <46077579+enorytk@users.noreply.github.com>
2021-10-04 21:27:20 -0400

    Passwords hashed

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -26,32 +26,29 @@ module.exports.postLogin = async (req, res, next) => {
 }
 
 module.exports.postSignUp = async (req, res, next) => {
-    const exist = await User.exists({
+      const exist = await User.exists({
         email: req.body.email
     });
 
     if(!exist) {
         const pass = req.body.password;
-        if(pass.length < 8 || !pass.match("^[A-Za-z0-9]")) { 
+        if(req.body.password.length < 8 || !req.body.password.match("^[A-Za-z0-9]")) { 
             res.send('Password must be at least 8 alphanumeric characters');
             return;
         }
         
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(pass, salt, function(err, hash) {
-                pass.replace(hash);
+        bcrypt.genSalt(10, function(err, salt) { 
+            bcrypt.hash(req.body.password, salt, async function(err, hash) {
+                const user = new User({
+                    email: req.body.email,
+                    password: hash
+                });
+
+                await user.save();
             });
         }); 
-
-        const user = new User({
-            email: req.body.email,
-            password: pass
-        })
-
-        console.log(user.email);
-        console.log(pass);
-        await user.save();
         
+        /*
         const token = new Token({
             _id: user.email,
             key: crypto.randomBytes(16).toString('hex')
@@ -65,7 +62,7 @@ module.exports.postSignUp = async (req, res, next) => {
                 return res.status(500).send({msg:'Technical Issue!, Please click on resend for verify your Email.'});
                 }
             return res.status(200).send('A verification email has been sent to ' + user.email + '. It will be expire after one day. If you not get verification Email click on resend.');
-        });
+        });*/
     }
     else { //redirect user to login or just tell them the email exists?
         res.send('An account already exists with the email ' + req.body.email + '.');
