b62c50aeac04b102f795cd5a90e17d6a26e9034d
enorytk <46077579+enorytk@users.noreply.github.com>
2021-10-04 22:47:07 -0400

    Update authController.js

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -26,43 +26,24 @@ module.exports.postLogin = async (req, res, next) => {
 }
 
 module.exports.postSignUp = async (req, res, next) => {
-      const exist = await User.exists({
+    const exist = await User.exists({
         email: req.body.email
     });
 
     if(!exist) {
-        const pass = req.body.password;
         if(req.body.password.length < 8 || !req.body.password.match("^[A-Za-z0-9]")) { 
-            res.send('Password must be at least 8 alphanumeric characters');
-            return;
+            return res.send('Password must be at least 8 alphanumeric characters');
         }
-        
+
         bcrypt.genSalt(10, function(err, salt) { 
             bcrypt.hash(req.body.password, salt, async function(err, hash) {
                 const user = new User({
                     email: req.body.email,
                     password: hash
                 });
-
                 await user.save();
             });
-        }); 
-        
-        /*
-        const token = new Token({
-            _id: user.email,
-            key: crypto.randomBytes(16).toString('hex')
         });
-        token.save();
-
-        var transporter = nodemailer.createTransport();
-        var mailOptions = { from: 'no-reply@nopus.com', to: user.email, subject: 'Account Verification Link', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + user.email + '\/' + token.token + '\n\nThank You!\n' };
-        transporter.sendMail(mailOptions, function (err) {
-            if (err) { 
-                return res.status(500).send({msg:'Technical Issue!, Please click on resend for verify your Email.'});
-                }
-            return res.status(200).send('A verification email has been sent to ' + user.email + '. It will be expire after one day. If you not get verification Email click on resend.');
-        });*/
     }
     else { //redirect user to login or just tell them the email exists?
         res.send('An account already exists with the email ' + req.body.email + '.');
