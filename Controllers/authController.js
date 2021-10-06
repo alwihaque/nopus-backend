3bb11e639fec439f269f7a3f3662a7343d2f2c3e
alwihaque <alwihaque@icloud.com>
2021-10-06 00:34:22 -0400

    Fixed postLogin Handler

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -6,22 +6,34 @@ const User = require('../Models/user');
 module.exports.postLogin = async (req, res, next) => {
     
     // Placeholder values for testing logic
-    const email = "vsknvlksvnlskv"; // I'm assuming the actual value will be in req.email ?
-    const password = "dvklnlkvkl";
-
+    const email = req.body.email; // I'm assuming the actual value will be in req.email ?
+    const password = req.body.password;
     // Query user information given email
-    const users = await User.findOne({"email":email});
+    try {
+        const user = await User.findOne({"email":email});
+        // Check if email exists
+        if (!user) return res.status(404).send("Email or password you entered was incorrect.");
+        // Check if password is correct
+        const match = await bcrypt.compare(password,user.password);
+        if(!match) {
+            return res.status(404).send("Email or password you entered was incorrect.");
+        }
+        // Check if user email is verified
+        if (!user.isVerified) {
+            return res.status(404).send("User is not verified. Please verify your email to login.");
+        }
+        res.status(200).json({
+            userId: user._id,
+            email: user.email
+        });
+    }
+    catch (e) {
+        console.log(e.message);
+    }
 
-    // Check if email exists
-    if (!users) return res.status(200).send("Email or password you entered was incorrect.");
-    // Check if password is correct
-    else if (users.email != email) return res.status(200).send("Email or password you entered was incorrect.");
-    // Check if user email is verified
-    else if (!users.isVerified) return res.status(200).send("User is not verified. Please verify your email to login.");
     // If all good, send to home page
-    else res.redirect('/home');
 
-    // TODO: 1. Add hashing for passwords 2. Return proper "errors" 3. Email verification/authentication
+
     // Questions: Do I need to implement something to make sure the method is POST? 
 }
 
--- Util/timer.js
+++ Util/timer.js
@@ -1,6 +1,6 @@
 const scraper = require("../Util/scrape");
 const cron = require('node-cron');
-module.exports.scrapeEvery12Hrs = async () => {
+module.exports.scrapeEvery12Hrs = () => {
     cron.schedule('0 */12 * * *', scraper.getData, {
         scheduled: true,
         timezone: "America/Sao_Paulo"
