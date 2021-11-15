ed2f7ca069d43cea19398aeb5eacd3457474633e
alwihaque <alwihaque@icloud.com>
2021-11-15 18:13:31 -0500

    All Routes Page

--- App.js
+++ App.js
@@ -16,7 +16,26 @@ app.use(profileRoute);
 app.use(authRoute);
 app.use(courseRoute);
 
+app.use("/", (req, res, next) => {
+    res.status(200).send(`<h1> Available Routes </h1>
+    <h2>For Profile Updates</h2>
+    <ul>
+    <li>PUT /profile/majorMinor/:uid</li>
+    <li>PUT /profile/courseList/:uid</li>
+    <li>PUT /profile/preferences/:uid</li>
+    </ul>
+    <h2>For Course Related Information</h2>
+    <ul>
+    <li>GET /home</li>
+    <li>GET  /home/id/:id</li>
+    <li>GET  /home/id/:id</li>
+    <li>GET  /home/:prefix</li>
+    <li>POST  /home/schedule</li> 
+</ul>
 
+
+`)
+})
 app.listen(process.env.PORT || 3000, async () => {
     await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ua0zj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`);
     logger.log('info', 'Connected to MongoDB');
