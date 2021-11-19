b6d8a0da29b42192887178bb5625b4f2cd294b77
alwihaque <alwihaque@icloud.com>
2021-11-19 16:07:43 -0500

    Cors again

--- App.js
+++ App.js
@@ -11,15 +11,7 @@ const logger = require('./Util/logger');
 scrape.scrapeEvery12Hrs();
 const app = express();
 app.use(helmet());
-app.use(function (req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    next();
-});
+app.use(cors());
 app.use(express.json());
 app.use(profileRoute);
 app.use(authRoute);
