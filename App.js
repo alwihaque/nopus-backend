87dc81525d124e1a5afc105b910be3649671c1f5
alwihaque <alwihaque@icloud.com>
2021-11-20 16:21:45 -0500

    dvnksvnlssdcsdcsdc

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const logger = require('./Util/logger');
 
 scrape.scrapeEvery12Hrs();
 const app = express();
-app.us(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(cors());
